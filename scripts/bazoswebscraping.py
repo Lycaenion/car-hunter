069eb4f79602ff72c5975eb06f56926cdaa1d110
Lycaenion <linn.martensson@hotmail.se>
2023-11-28 10:57:41 +0100

    changed to selenium

---
+++ scripts/bazoswebscraping.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+import selenium
+
+driver = webdriver.Chrome()
+
+driver.get("https://auto.bazos.sk/ford/?hledat=galaxy&rubriky=auto&hlokalita=97411&humkreis=300&cenaod=&cenado=&order=&crp=&kitx=ano")
+
+
+ads = driver.find_elements(By.CSS_SELECTOR, 'div.inzeraty.inzeratyflex')
+
+for ad in ads:
+
+    print(ad.find_element(By.CLASS_NAME, 'inzeratynadpis').find_element(By.CLASS_NAME, 'nadpis').text)
