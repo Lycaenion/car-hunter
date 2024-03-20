d271791ca52eb3d1bfdc8ac4ae19a6701569ba2a
Lycaenion <linn.martensson@hotmail.se>
2024-03-20 16:04:15 +0100

    program now scrapes for 3 models of cars, saves to db and sends email

--- scripts/bazoswebscraping.py
+++ scripts/bazoswebscraping.py
@@ -6,44 +6,66 @@ import re
 import db
 import emailsender
 
-def find_cars(url):
-
+def find_cars():
+    urls = ["https://auto.bazos.sk/ford/?hledat=galaxy&rubriky=auto&hlokalita=97411&humkreis=300&cenaod=&cenado=&order=&crp=&kitx=ano",
+              "https://auto.bazos.sk/seat/?hledat=alhambra&rubriky=auto&hlokalita=97411&humkreis=300&cenaod=&cenado=&order=&crp=&kitx=ano",
+              "https://auto.bazos.sk/volkswagen/?hledat=sharan&rubriky=auto&hlokalita=97411&humkreis=300&cenaod=&cenado=&order=&crp=&kitx=ano"]
     link_list = []
     email_body = ""
-    driver = webdriver.Chrome()
-    bazos_url = url
 
+    for url in urls:
+        print(url)
+        scrape_bazos(url, link_list)
+
+    if len(link_list) == 0:
+        email_body = "No new ads to view"
+    else:
+        for link in link_list:
+            email_body = email_body + link + '\n'
+
+    emailsender.sendemail(email_body)
+
+def scrape_bazos(bazos_url, link_list):
+
+    driver = webdriver.Chrome()
+    print("I'm here")
     driver.get(bazos_url)
     ads = driver.find_elements(By.CSS_SELECTOR, 'div.inzeraty.inzeratyflex')
     YEAR_PATTERN = re.compile(r'(?sm).*?(?P<year>\b20[12]\d\b)')
-    car_model = ""
 
     for ad in ads:
 
         link = ad.find_element(By.CLASS_NAME, 'inzeratynadpis').find_element(By.TAG_NAME, 'a').get_attribute("href")
-        print("Link " + link)
+
         if "galaxy" in bazos_url:
             car_brand = "Ford"
             car_model = "Galaxy"
+        if "alhambra" in bazos_url:
+            car_brand = "Seat"
+            car_model = "Alhambra"
+        if "sharan" in bazos_url:
+            car_brand = "Volkswagen"
+            car_model = "Sharan"
 
         if db.is_ad_in_db(link) is False:
-                print("The ad is not in the db")
-                price = ad.find_element(By.CLASS_NAME, "inzeratycena").find_element(By.TAG_NAME, "b").text
-                if re.search('[a-zA-z]', price) is not None:
-                    print("Not a float")
-                else:
-                    converted_price = float(price.replace(" ", "").strip('€'))
-                    print(f'the price is {price}')
-                    title = ad.find_element(By.CLASS_NAME, 'inzeratynadpis').find_element(By.CLASS_NAME, "nadpis").text
-                    ad.find_element(By.LINK_TEXT, title).click()
-                    long_description = driver.find_element(By.CLASS_NAME, "popisdetail").text
-                    driver.back()
-                    match = YEAR_PATTERN.match(long_description)
-                    if match:
-                        year = match.group('year')
-                        #print(f'title {title} year {year} link {link} price {price} long description {long_description}')
-                        db.add_ad_to_db(link, converted_price, car_brand, car_model)
-                        link_list.append(link)
+
+            print("The ad is not in the db")
+            price = ad.find_element(By.CLASS_NAME, "inzeratycena").find_element(By.TAG_NAME, "b").text
+            if re.search('[a-zA-z]', price) is not None:
+                print("Not a float")
+            else:
+                converted_price = float(price.replace(" ", "").strip('€'))
+                print(f'the price is {price}')
+                title = ad.find_element(By.CLASS_NAME, 'inzeratynadpis').find_element(By.CLASS_NAME, "nadpis").text
+                ad.find_element(By.LINK_TEXT, title).click()
+                long_description = driver.find_element(By.CLASS_NAME, "popisdetail").text
+                driver.back()
+                match = YEAR_PATTERN.match(long_description)
+                if match:
+                    year = match.group('year')
+                    db.add_ad_to_db(link, converted_price, car_brand, car_model)
+                    link_list.append(link)
+
 
         else:
             print("ad already in db")
@@ -54,13 +76,7 @@ def find_cars(url):
         #Give the program time to reload all the elements before starting the next one
         time.sleep(2)
 
-    if len(link_list) == 0:
-        email_body = "No new ads to view"
-    else:
-        for link in link_list:
-            email_body = email_body + link + '\n'
-
-    #emailsender.sendemail(email_body)
+    return link_list
 
 if __name__ == "__main__":
-    find_cars("https://auto.bazos.sk/ford/?hledat=galaxy&rubriky=auto&hlokalita=97411&humkreis=300&cenaod=&cenado=&order=&crp=&kitx=ano")
+    find_cars()
